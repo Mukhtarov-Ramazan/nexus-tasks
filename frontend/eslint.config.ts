acd93438db01ba1e55b8a088f53e9998d1bf3238
Mukhtarov-Ramazan <mukhtarovramazan239@gmail.com>
2025-10-19 21:06:59 +0300

    Насройка линтеров

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,22 +1,42 @@
+import { defineConfig } from 'eslint'
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
-export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+export default defineConfig({
+  name: 'app/files-to-lint',
+  files: ['**/*.{ts,mts,tsx,vue}'],
+  ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  extends: [
+    ...vueTsConfigs.recommended.extends,
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['vue'],
+  rules: {
+    'vue/no-unused-vars': 'error',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+      alphabetize: { order: 'asc' },
+    }],
   },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  skipFormatting,
-)
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  // включаем конфиги из плагинов
+  settings: {
+    'vue/setup-compiler-macros': true
+  }
+})
